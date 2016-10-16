e0fbe13684ce2f4c2fa8d9ba7ddb8bde345a28c0
VTEC <527409987@qq.com>
2016-10-17 00:35:10 +0800

    pc端主体完成

--- js/index.js
+++ js/index.js
@@ -2,6 +2,27 @@
  * Created by VTEC on 2016/10/14.
  */
 $(function () {
+    //down闪光灯效果
+    time_down=setInterval(function () {
+        autoPlay($('.down'))
+    },800)
+    function autoPlay(Obj){
+        Obj.toggleClass('yellow')
+    }
+
+    //二维码显示
+    $('.tel-1').on('mouseover', function () {
+        $('.erwei').css({
+            display:'block'
+        })
+    })
+    $('.tel-1').on('mouseout', function () {
+        $('.erwei').css({
+            display:'none'
+        })
+    })
+
+    //个人简介上下拉效果
     $('.down').on('click', function () {
         $('.introduce').slideDown(function () {
             $('.down').css({
