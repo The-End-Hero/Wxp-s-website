1c6962e1cb9325c1ca8d170f6f24846494366261
VTEC <527409987@qq.com>
2016-10-18 12:47:38 +0800

    手风琴完成

--- js/index.js
+++ js/index.js
@@ -52,8 +52,16 @@ $(function () {
                 lis_ind[j].firstChild.style.width='0%'
             }
             this.firstChild.style.width='100%'
+            this.lastChild.style.opacity='1'
+            this.lastChild.style.fontSize='14px'
             this.parentNode.style.zIndex=++num_ind+''
         }
-
+        lis_ind[i].onmouseout= function () {
+            for(var k=0;k<lis_ind.length;k++){
+                lis_ind[k].firstChild.style.width='0%'
+                lis_ind[k].lastChild.style.opacity='0.5'
+                lis_ind[k].lastChild.style.fontSize='12px'
+            }
+        }
     }
 })
