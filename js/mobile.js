afcc7b648c5d5738c2824573c9879f1d3e5b7ea9
VTEC <527409987@qq.com>
2016-10-23 20:18:51 +0800

    mobilephp

---
+++ js/mobile.js
@@ -0,0 +1,7 @@
+$(function () {
+    $.getJSON('./loopPic.php', function (data) {
+        console.log(data)
+        var str=template('loop',{list:data})
+        $('#loop-pic').html(str);
+    })
+})
---
+++ loopPic.php
@@ -0,0 +1,4 @@
+<?php
+echo file_get_contents('info/mobile_loop.json');
+
+?>
