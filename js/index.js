f08d21447d21f99cb21f0625b0ff52fed542256d
The-End-Hero <527409987@qq.com>
2016-11-03 02:17:25 +0800

    SPAwork

--- js/index.js
+++ js/index.js
@@ -5,47 +5,52 @@ $(function () {
     //jsonp天气
     $.getJSON('http://api.jirengu.com/weather.php', function (data) {
         console.log(data)
-        var str_weather=template('weather',{list:data})
+        var str_weather = template('weather', {list: data})
         $('.weather').html(str_weather)
         //jsonp事件
         $('.weather span').hide()
         $('.weather p').on('mouseenter', function () {
-            $('.weather span').stop(true,true).show(500)
+            $('.weather span').stop(true, true).show(500)
         })
         $('.weather p').on('mouseleave', function () {
-            $('.weather span').stop(true,true).hide(500)
+            $('.weather span').stop(true, true).hide(500)
         })
     })
     //头像
     $('.me').on('mouseenter', function () {
         $('.me span').css({
-            left:'100%'
+            left: '100%'
         })
     })
     $('.me').on('mouseleave', function () {
         $('.me span').css({
-            left:'-100%'
+            left: '-100%'
         })
     })
     //闪光灯效果
-    var time_down=setInterval(function () {
+    var time_down = setInterval(function () {
         autoPlay($('.down'))
-    },500)
-    function autoPlay(Obj){
+    }, 500)
+    var time_h = setInterval(function () {
+        autoPlay($('.top>h4'))
+    }, 800)
+    var time_left=setInterval(function () {
+        autoPlay($('.left'))
+    },1000)
+    function autoPlay(Obj) {
         Obj.toggleClass('yellow')
     }
-    var time_h=setInterval(function () {
-        autoPlay($('.top>h4'))
-    },800)
+
+
     //二维码显示
     $('.tel-1').on('mouseover', function () {
         $('.erwei').css({
-            display:'block'
+            display: 'block'
         })
     })
     $('.tel-1').on('mouseout', function () {
         $('.erwei').css({
-            display:'none'
+            display: 'none'
         })
     })
 
@@ -53,41 +58,65 @@ $(function () {
     $('.down').on('click', function () {
         $('.introduce').slideDown(function () {
             $('.down').css({
-                opacity:'0'
+                opacity: '0'
             })
             $('.up').css({
-                opacity:'1'
+                opacity: '1'
             })
         });
     })
     $('.up').on('click', function () {
         $('.introduce').slideUp(function () {
             $('.down').css({
-                opacity:'1'
+                opacity: '1'
             })
             $('.up').css({
-                opacity:'0'
+                opacity: '0'
             })
         });
     })
+    //.left拉出效果
+    $('.left').on('click', function () {
+        $('#spa').show()
+        $('#spa').animate({
+            width:'100%'
+        },1000, function () {
+            $('#spa>.spa_bottom').show()
+            $('#spa>.spa_bottom').animate({
+                width:'100%'
+            },1000, function () {
+                //console.log($('#spa>.spa_bottom').width())
+                //spa_left
+                if($('#spa>.spa_bottom').width()==$('body').width()){
+                    //console.log('asd')
+                    $('#spa>.spa_left').show()
+                    $('#spa>.spa_left').animate({
+                        height:'100%'
+                    })
+                }
+            })
+
+        })
+    })
+
     //个人简介手风琴效果
-    var lis_ind=document.querySelectorAll('.shou')
-    var num_ind=2
-    for(var i= 0;i<lis_ind.length;i++){
-        lis_ind[i].onmouseover= function () {
-            for(var j=0;j<lis_ind.length;j++){
-                lis_ind[j].firstChild.style.width='0%'
+    var lis_ind = document.querySelectorAll('.shou')
+    var num_ind = 2
+    for (var i = 0; i < lis_ind.length; i++) {
+        lis_ind[i].onmouseover = function () {
+            for (var j = 0; j < lis_ind.length; j++) {
+                lis_ind[j].firstChild.style.width = '0%'
             }
-            this.firstChild.style.width='100%'
-            this.lastChild.style.opacity='1'
-            this.lastChild.style.fontSize='14px'
-            this.parentNode.style.zIndex=++num_ind+''
+            this.firstChild.style.width = '100%'
+            this.lastChild.style.opacity = '1'
+            this.lastChild.style.fontSize = '14px'
+            this.parentNode.style.zIndex = ++num_ind + ''
         }
-        lis_ind[i].onmouseout= function () {
-            for(var k=0;k<lis_ind.length;k++){
-                lis_ind[k].firstChild.style.width='0%'
-                lis_ind[k].lastChild.style.opacity='0.5'
-                lis_ind[k].lastChild.style.fontSize='12px'
+        lis_ind[i].onmouseout = function () {
+            for (var k = 0; k < lis_ind.length; k++) {
+                lis_ind[k].firstChild.style.width = '0%'
+                lis_ind[k].lastChild.style.opacity = '0.5'
+                lis_ind[k].lastChild.style.fontSize = '12px'
             }
         }
     }
--- js/mobile.js
+++ js/mobile.js
@@ -2,129 +2,150 @@ $(function () {
     //get获取图片数据
     $.getJSON('./loopPic.php', function (data) {
         //console.log(data)
-        var str=template('loop',{list:data})
+        var str = template('loop', {list: data})
         $('#loop-pic').html(str);
         loop()
     })
-    function loop(){
+    function loop() {
         //轮播图开始
         //轮播图的ul
-        var loopUl=$('#loopUl')
+        var loopUl = $('#loopUl')
         //轮播图索引的ul
-        var indexUl=$('#indexUl')
+        var indexUl = $('#indexUl')
         indexUl.children('li').eq(0).addClass('black')
         //屏幕的宽度,因为rem布局所以10rem就是100%宽度
         //var loopWidth=$('#loop-pic').width();
         //使用定时器实现无限轮播
-        var index=1;
+        var index = 1;
         //transform实现动画
-        var autoMove= function () {
+        var autoMove = function () {
             //console.log('b')
             loopUl.animate({
-                'transform':'translateX('+index*-10+'rem)',
-                'webkitTransform':'translateX('+index*-10+'rem)'
-            },400,'ease', function () {
+                'transform': 'translateX(' + index * -10 + 'rem)',
+                'webkitTransform': 'translateX(' + index * -10 + 'rem)'
+            }, 400, 'ease', function () {
                 //animate回调函数,结束时候执行
-                if(index>=6){
-                    index=1;
+                if (index >= 6) {
+                    index = 1;
                     //
                     loopUl.css({
-                        'transform':'translateX('+index*-10+'rem)',
-                        'webkitTransform':'translateX('+index*-10+'rem)'
+                        'transform': 'translateX(' + index * -10 + 'rem)',
+                        'webkitTransform': 'translateX(' + index * -10 + 'rem)'
                     })
-                }else if(index<=0){
-                    index=5
+                } else if (index <= 0) {
+                    index = 5
                     loopUl.css({
-                        'transform':'translateX('+index*-10+'rem)',
-                        'webkitTransform':'translateX('+index*-10+'rem)'
+                        'transform': 'translateX(' + index * -10 + 'rem)',
+                        'webkitTransform': 'translateX(' + index * -10 + 'rem)'
                     })
                 }
                 //改变索引ul
-                indexUl.children('li').removeClass('black').eq(index-1).addClass('black')
+                indexUl.children('li').removeClass('black').eq(index - 1).addClass('black')
 
-                if(timerId==undefined){
-                    timerId=setInterval(function () {
+                if (timerId == undefined) {
+                    timerId = setInterval(function () {
                         index++;
                         autoMove()
-                    },1000)
+                    }, 1000)
                 }
             })
         }
         //开启定时器
-        var timerId=setInterval(function () {
+        var timerId = setInterval(function () {
             index++
             //console.log('aaaa')
             autoMove()
-        },1000)
+        }, 1000)
         //手指滑动事件
         $('#loop-pic').on('swipeLeft', function (e) {
             //console.log(e)
-            e.preventDefault();
-            clearInterval(timerId)
-            timerId=undefined
-            index++
-            autoMove()
+            //e.preventDefault();
+            //if(deltaX>$('#loop-pic').width()/5){
+                clearInterval(timerId)
+                timerId = undefined
+                index++
+                autoMove()
+            //}
+
         })
         $('#loop-pic').on('swipeRight', function (e) {
-            e.preventDefault();
-            clearInterval(timerId)
-            timerId=undefined
-            index--
-            autoMove()
+            //e.preventDefault();
+            //if(deltaX>$('#loop-pic').width()/5){
+                clearInterval(timerId)
+                timerId = undefined
+                index--
+                autoMove()
+            //}
         })
+        //var ul
+        //var startX = 0;
+        //var moveX =0;
+        //var distanceX = 0;
         $('#loop-pic').on('touchstart', function (e) {
             e.preventDefault();
+            //var touch = e.touches[0];
+            //return startPosition=touch.pageX
+                //y: touch.pageY
+
+            //startX= e.touches[0].clientX
         })
         $('#loop-pic').on('touchmove', function (e) {
             e.preventDefault();
+            //moveX = e.touches[0].clientX - startX;
         })
         //$('#loop-pic').on('touchend', function (e) {
         //    e.preventDefault();
+            //var touch = e.touches[0];
+            //return endPosition = touch.pageX
+
         //})
-
+        //var deltaX = Math.abs(endPosition - startPosition);
+        //deltaY = endPosition.y - startPosition.y;
+        //moveLength = Math.sqrt(Math.pow(Math.abs(deltaX), 2) + Math.pow(Math.abs(deltaY), 2));
     }
-    top()
-    function top(){
-        var top=document.querySelector('.top')
-        top.style.backgroundColor='rgba(0,0,0,0)'
-        //轮播图高度
-        var loopH=document.querySelector('#loop-pic').offsetHeight
-        window.onscroll= function (e) {
-            var percent=window.document.body.scrollTop/loopH
 
-            if(percent>1){
-                percent=1
+    top()
+    function top() {
+        var top = document.querySelector('.top')
+        top.style.backgroundColor = 'rgba(0,0,0,0)'
+        //轮播图高度
+        var loopH = document.querySelector('#loop-pic').offsetHeight
+        window.onscroll = function (e) {
+            var percent = window.document.body.scrollTop / loopH
+
+            if (percent > 1) {
+                percent = 1
             }
-            top.style.backgroundColor='rgba(0,0,0,'+percent+')'
+            top.style.backgroundColor = 'rgba(0,0,0,' + percent + ')'
         }
     }
-    
-    
+
+
     $.getJSON('./info/mobile_1.json', function (data) {
         //console.log(data[0].ifm)
-        var str=template('ifm',{list:data})
+        var str = template('ifm', {list: data})
         $('.ajax-8').html(str)
     })
     //注册事件
-    var navs=document.querySelectorAll('.pic>div')
-    for(var i=0;i<navs.length;i++){
+    var navs = document.querySelectorAll('.pic>div')
+    for (var i = 0; i < navs.length; i++) {
 
         $(navs[i]).on('touchend', function (e) {
             //console.log(e.changedTouches[0])
-            var page_X=e.changedTouches[0].pageX//鼠标所在位置X
-            var page_Y=e.changedTouches[0].pageY//鼠标所在位置Y
-            var offset_left=$(this).offset().left//外盒子在浏览器中的位置
-            var offset_top=$(this).offset().top//外盒子在浏览器中的位置
+            var page_X = e.changedTouches[0].pageX//鼠标所在位置X
+            var page_Y = e.changedTouches[0].pageY//鼠标所在位置Y
+            var offset_left = $(this).offset().left//外盒子在浏览器中的位置
+            var offset_top = $(this).offset().top//外盒子在浏览器中的位置
 
             //水波纹效果
-            var span=$('<span class="water"></span>').appendTo($(this))
+            var span = $('<span class="water"></span>').appendTo($(this))
             $(span).hide()
             //console.log($(span).height())
-            if((page_X-offset_left)>0&&(page_X-offset_left)<$(this).width()&&(page_Y-offset_top)>0&&(page_Y-offset_top)<$(this).height()){
+            if ((page_X - offset_left) > 0 && (page_X - offset_left) < $(this).width() && (page_Y - offset_top) > 0 && (page_Y - offset_top) < $(this).height()) {
 
                 $(span).css({
-                    left:(page_X- offset_left)-40+'px',
-                    top:(page_Y- offset_top)-40+'px',
+                    left: (page_X - offset_left) - 40 + 'px',
+                    top: (page_Y - offset_top) - 40 + 'px',
                 })
                 $(span).fadeIn()
                 $(span).addClass('animated zoomIn')
@@ -135,23 +156,22 @@ $(function () {
                     $(span).hide()
                     $(span).remove()
                     //$(span).removeClass('animated zoomIn')
-                },400)
+                }, 400)
                 //setTimeout(function () {
                 //    $(span).remove()
                 //},800)
             }
-            changeTab($(this).index()+1)
+            changeTab($(this).index() + 1)
             //.index()天坑啊,如果是选择器是返回相对兄弟元素的位置,如果是dom元素集合是返回第一个元素的位置.....
         })
     }
-    var changeTab= function (n) {
-        $.getJSON('./info/mobile_'+n+'.json', function (data) {
+    var changeTab = function (n) {
+        $.getJSON('./info/mobile_' + n + '.json', function (data) {
             //console.log(data)
-            var str=template('ifm',{list:data})
+            var str = template('ifm', {list: data})
             $('.ajax-8').html(str)
         })
     }
-
 
 
 })
