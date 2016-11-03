5db67d611c858926ac8d289608a507f84a888e47
The-End-Hero <527409987@qq.com>
2016-11-04 01:31:53 +0800

    修复获取地理位置

--- js/index.js
+++ js/index.js
@@ -2,20 +2,109 @@
  * Created by VTEC on 2016/10/14.
  */
 $(function () {
-    //jsonp天气
-    $.getJSON('http://api.jirengu.com/weather.php', function (data) {
-        console.log(data)
-        var str_weather = template('weather', {list: data})
-        $('.weather').html(str_weather)
-        //jsonp事件
-        $('.weather span').hide()
-        $('.weather p').on('mouseenter', function () {
-            $('.weather span').stop(true, true).show(500)
+    //动态获取城市和天气
+    jQuery.getScript("http://int.dpool.sina.com.cn/iplookup/iplookup.php?format=js", function () {
+        province = remote_ip_info["province"];
+        city = remote_ip_info["city"];
+        $.ajax({
+            url: "http://api.map.baidu.com/telematics/v3/weather?location=" + city + "&output=json&ak=KawiLiQFG552sBQnCWNtv9iluXqrUY12",
+            dataType: 'jsonP',
+            success: function (data) {
+                // 读取对象
+                console.log(data)
+                var str_weather = template('weather', {list: data})
+                $('.weather').html(str_weather)
+                //jsonp事件
+                $('.weather span').hide()
+                $('.weather p').on('mouseenter', function () {
+                    $('.weather span').stop(true, true).show(500)
+                })
+                $('.weather p').on('mouseleave', function () {
+                    $('.weather span').stop(true, true).hide(500)
+                })
+                $('#weather_charts').on('click', function () {
+                    var weatherResults = data.results[0];
+                    console.log(weatherResults)
+                    var data0 = weatherResults.weather_data[0].date
+                    var data1 = weatherResults.weather_data[1].date
+                    var data2 = weatherResults.weather_data[2].date
+                    var data3 = weatherResults.weather_data[3].date
+                    console.log(data0)
+                    console.log(data1)
+                    console.log(data2)
+                    console.log(data3)
+                    var wendu0 = weatherResults.weather_data[0].temperature.slice(0, 2)
+                    var wendu1 = weatherResults.weather_data[1].temperature.slice(0, 2)
+                    var wendu2 = weatherResults.weather_data[2].temperature.slice(0, 2)
+                    var wendu3 = weatherResults.weather_data[3].temperature.slice(0, 2)
+                    console.log(wendu0)
+                    console.log(wendu1)
+                    console.log(wendu2)
+                    console.log(wendu3)
+                    var wendu4 = weatherResults.weather_data[0].temperature.slice(5, 7)
+                    var wendu5 = weatherResults.weather_data[1].temperature.slice(5, 7)
+                    var wendu6 = weatherResults.weather_data[2].temperature.slice(5, 7)
+                    var wendu7 = weatherResults.weather_data[3].temperature.slice(5, 7)
+                    console.log(wendu4)
+                    console.log(wendu5)
+                    console.log(wendu6)
+                    console.log(wendu7)
+                    var option = {
+                        title: {
+                            text: city+'最近天气'
+                        },
+                        tooltip: {
+                            trigger: 'axis'
+                        },
+                        legend: {
+                            data: ['最高温度', '最低温度'],
+//					backgroundColor:'rgba(0,0,0,.4)'
+                        },
+                        xAxis: {       //直角坐标系 grid 中的 x 轴
+                            type: 'category',
+                            boundaryGap: false,
+                            data: [data0, data1, data2, data3]
+                        },
+                        yAxis: {       //直角坐标系 grid 中的 y 轴
+                            type: 'value',
+                            name: '℃'
+                        },
+//				yAxis: {},
+//				series: [{
+//					name: '销量',
+//					type: 'bar',
+//					data: [5, 20, 36, 10, 10, 20]
+//				}]
+                        grid: {
+                            left: '10%',
+                            right: '4%',
+                            bottom: '3%',
+                            containLabel: true
+                        },
+                        series: [      //系列列表
+                            {
+                                name: '最高温度',
+                                type: 'line',
+//						stack: '最高',
+//						symbol:'pin',
+                                data: [wendu0, wendu1, wendu2, wendu3]
+                            },
+                            {
+                                name: '最低温度',
+                                type: 'line',
+//						stack: '最低',
+                                data: [wendu4, wendu5, wendu6, wendu7]
+                            },
+                        ],
+//				backgroundColor:'rgba(0,0,228,.5)'
+                    };
+                    var myChart = echarts.init(document.getElementById('main'));
+                    myChart.setOption(option);
+                })
+
+            }
         })
-        $('.weather p').on('mouseleave', function () {
-            $('.weather span').stop(true, true).hide(500)
-        })
-    })
+    });
     //头像
     $('.me').on('mouseenter', function () {
         $('.me span').css({
@@ -96,6 +185,24 @@ $(function () {
                 }
             })
 
+        })
+    })
+    //spa_right效果
+    $('.spa_right').on('click', function () {
+        $('#spa>.spa_left').animate({
+            height:'0'
+        }, function () {
+            $('#spa>.spa_bottom').animate({
+                width:'0'
+            }, function () {
+                $('#spa').animate({
+                    width:'0'
+                }, function () {
+                    $('#spa>.spa_left').hide()
+                    $('#spa>.spa_bottom').hide()
+                    $('#spa').hide()
+                })
+            })
         })
     })
 
